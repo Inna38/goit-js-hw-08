800af07e68f2baa1914dcbc517bb876a861908b3
Inna Didenko <innadidenko29@gmail.com>
2023-07-01 18:33:05 +0300

    update task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+let gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item" >
+   <a class="gallery__link" href="${original}" >
+      <img class="gallery__image" src="${preview} "alt="${description}"/>   
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+const form = document.querySelector("form")
 
+
+form.addEventListener("input", onFormInput)
+form.addEventListener("submit", onFormSubmit)
+
+let userForm = {}
+  
+function onFormInput(e) {
+   
+    let {email, message} = e.currentTarget.elements
+
+ userForm  = {
+        email:  email.value,
+        message: message.value,
+    }
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(userForm))
+
+
+
+   
+
+}
+
+function saveUserLocal(userForm) {
+
+        const saveUser = JSON.parse(localStorage.getItem("feedback-form-state")); 
+    if (saveUser) {
+        console.log(saveUser);
+        userForm.email = saveUser.email;
+ 
+    }
+    //  console.log(userForm);
+    // const saveUser = JSON.parse(localStorage.getItem("feedback-form-state")); 
+    // if (saveUser) {
+    //   userForm.value = saveUser.email;
+    //      console.log(saveUser.message);
+    // }
+   
+}
+
+function onFormSubmit(e) {
+e.preventDefault();
+    e.target.reset() 
+     console.log(userForm);
+}
+
+ saveUserLocal(userForm)
+ 
+
+    // _.throttle(func, [wait=0], [options={}])
+    // if (localStorage.getItem("feedback-form-state")) {
+  
+    // }
