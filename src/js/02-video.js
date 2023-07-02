e6084722ddb3503ca0db1c9de765c402d72c8fe6
Inna Didenko <innadidenko29@gmail.com>
2023-07-02 23:29:14 +0300

    update task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
 
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle"
 
 const iframe = document.querySelector('iframe');
- const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-//     player.on('play', function() {
-//         console.log('played the video!');
-//     });
+player.on('timeupdate',throttle(onStart, 1000));
 
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
+function onStart(time) {
+ localStorage.setItem("videoplayer-current-time", time.seconds)
+}
+
+ player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,55 +1,49 @@
-const form = document.querySelector("form")
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
 
-form.addEventListener("input", onFormInput)
-form.addEventListener("submit", onFormSubmit)
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
-let userForm = {}
-  
+let userForm = {};
+
 function onFormInput(e) {
-   
-    let {email, message} = e.currentTarget.elements
+  let { email, message } = e.currentTarget.elements;
 
- userForm  = {
-        email:  email.value,
-        message: message.value,
-    }
+  userForm = {
+    email: email.value,
+    message: message.value,
+  };
 
-    localStorage.setItem("feedback-form-state", JSON.stringify(userForm))
+  localStorage.setItem('feedback-form-state', JSON.stringify(userForm));
 
+  const saveUser = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (saveUser) {
+     console.log('saveUser', saveUser.email);
+     console.log('email.value', email.value);
 
+    email.value = saveUser.email;
+    message.value = saveUser.message;
+  }
 
-   
-
+  //  saveUserLocal(userForm);
 }
 
-function saveUserLocal(userForm) {
+// function saveUserLocal(userForm) {
 
-        const saveUser = JSON.parse(localStorage.getItem("feedback-form-state")); 
-    if (saveUser) {
-        console.log(saveUser);
-        userForm.email = saveUser.email;
- 
-    }
-    //  console.log(userForm);
-    // const saveUser = JSON.parse(localStorage.getItem("feedback-form-state")); 
-    // if (saveUser) {
-    //   userForm.value = saveUser.email;
-    //      console.log(saveUser.message);
-    // }
-   
-}
+// //     const saveUser = JSON.parse(localStorage.getItem('feedback-form-state'));
+// //   if (saveUser) {
+// //      console.log("saveUser",saveUser.email);
+
+// //  console.log("email.value", email.value);
+// //   userForm.email = saveUser.email;
+// //  message.value = saveUser.message;
+// //   }
+
+// }
 
 function onFormSubmit(e) {
-e.preventDefault();
-    e.target.reset() 
-     console.log(userForm);
+  e.preventDefault();
+  e.target.reset();
+  console.log(userForm);
 }
-
- saveUserLocal(userForm)
- 
-
-    // _.throttle(func, [wait=0], [options={}])
-    // if (localStorage.getItem("feedback-form-state")) {
-  
-    // }
