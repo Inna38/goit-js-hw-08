7e4e55b9d27ba27b6b1c06fa6e07920a815fdd8a
Inna Didenko <innadidenko29@gmail.com>
2023-07-03 18:30:07 +0300

    update task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,37 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
-
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
-
-let userForm = {};
 
 function onFormInput(e) {
   let { email, message } = e.currentTarget.elements;
 
-  userForm = {
+  let userForm = {
     email: email.value,
     message: message.value,
   };
 
   localStorage.setItem('feedback-form-state', JSON.stringify(userForm));
-
-  const saveUser = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (saveUser) {
-     console.log('saveUser', saveUser.email);
-     console.log('email.value', email.value);
-
-    email.value = saveUser.email;
-    message.value = saveUser.message;
-  }
-
-  //  saveUserLocal(userForm);
 }
 
-// function saveUserLocal(userForm) {
+function saveUserLocal(e) {
+  const saveUser = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-// //     const saveUser = JSON.parse(localStorage.getItem('feedback-form-state'));
-// //   if (saveUser) {
-// //      console.log("saveUser",saveUser.email);
-
-// //  console.log("email.value", email.value);
-// //   userForm.email = saveUser.email;
-// //  message.value = saveUser.message;
-// //   }
-
-// }
+  if (saveUser) {
+    message.value = saveUser.message;
+    email.value = saveUser.email;
+  }
+}
 
 function onFormSubmit(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   e.preventDefault();
-  e.target.reset();
-  console.log(userForm);
+  form.reset();
+
+  localStorage.removeItem('feedback-form-state');
 }
+
+saveUserLocal();
